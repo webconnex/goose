9478b88790fd6cef0c83a62703e37987be06ff0a
Daniel Riley <daniel@webconnex.io>
2021-03-03 14:53:04 -0800

    Revert "update goose"

--- cmd/goose/driver_no_mysql.go
+++ cmd/goose/driver_no_mysql.go
@@ -2,6 +2,11 @@
 
 package main
 
-func normalizeDBString(driver string, str string, certfile string) string {
+import (
+	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/ziutek/mymysql/godrv"
+)
+
+func normalizeDBString(driver string, str string) string {
 	return str
 }
